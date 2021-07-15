07edff45765eab28df93873b766a3a405c8d68a3
Yashpreet Kaur <80843222+DamYash@users.noreply.github.com>
2021-07-15 12:25:27 -0400

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,34 @@
+package Assignment_6;
+//Run-time Polymorphism example:
+class Animal
+{
+	public void move()
+	{
+		System.out.println("Animals can move");
+	}
+}
+class Dog extends Animal
+{
+	public void move()
+	{
+		System.out.println("Dogs can walk and run");
+	}
+}
+public class TestDog 
+{
+	public static void main(String[] args) 
+	{
+		Animal a = new Animal();
+		Animal b = new Dog();
+		a.move();
+		b.move();
+	}
+}
+
+
+/* Output:
+
+Animals can move
+Dogs can walk and run
+
+*/
